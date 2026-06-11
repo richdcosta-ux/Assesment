8da7071de6dc424a598a05e72774d25942415571
richdcosta-ux <rich.dcosta@cathcollege.school.nz>
2026-06-11 13:25:43 +1200

    added speed

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -4,21 +4,14 @@ extends CharacterBody2D
 var basicattack = false;
 
 var movement = Vector2();
-const SPEED = 300.0
+@export var walk_speed = 200.0
+@export_range(0, 1) var acceleration = 0.1
+@export_range(0, 1) var deceleration = 0.1
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	
-	if (velocity.x > 1 || velocity.x < -1) && basicattack == false:
-		$player_animation.animation = "run"
-	
-	else:
-		movement.x = 0;
-		if basicattack == false:
-			$player_animation.animation = "idle"
-		
-	
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -26,28 +19,30 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor() && basicattack == false:
 		velocity.y = JUMP_VELOCITY
-		
-		
-		
-	if Input.is_action_just_pressed("attack"):
-		$player_animation.play("slash")
-		basicattack == true;
-		$attackarea/CollisionShape2D.disabled == false;
-		
+	
+	if Input.is_action_pressed("attack"):
+		player_attack()
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction  && basicattack == false:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * walk_speed, walk_speed * acceleration)
+		$player_animation.play("run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
+		$player_animation.play("idle")
 	move_and_slide()
 	
 	var isleft = velocity.x < 0 
 	$player_animation.flip_h = isleft
 	
 
+func player_attack():
+	$player_animation.play("slash")
+	basicattack == true
+	$attackarea/CollisionShape2D.disabled == false
+	await $player_animation.animation_finished
 
 func _on_player_animation_animation_finished() -> void:
 	if $player_animation.animation == "slash":
