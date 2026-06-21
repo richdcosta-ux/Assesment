a1c2849e57df0183b613c4c0843c8f8148231505
richdcosta-ux <rich.dcosta@cathcollege.school.nz>
2026-06-21 21:20:20 +1200

    added 2x jumps and wall clinging

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var player_animation = $player_animation
 @onready var shape_cast_2d: ShapeCast2D = $ShapeCast2D
+@onready var sprite = $player_animation
 
 @export var walk_speed = 150.0
 @export var run_speed = 250.0
@@ -21,6 +22,11 @@ var is_dashing = false
 var dash_start_position = 0
 var dash_direction = 0
 var dash_timer = 0
+var jumps_left = 1 
+var on_wall = false
+var wall_direction = 0
+var is_wall_clinging = false
+
 
 func _physics_process(delta: float) -> void:
 	if dash_timer > 0: 
@@ -29,14 +35,30 @@ func _physics_process(delta: float) -> void:
 		return
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if on_wall and velocity.y > 0:
+			velocity.y = min(velocity.y, 80)
+		else:
+			velocity += get_gravity() * delta
+	if is_on_floor():
+		jumps_left = 1
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor() and not basicattack:
-		velocity.y = jump_force
+	if Input.is_action_just_pressed("jump") and not basicattack:
+		if is_on_floor():
+			velocity.y = jump_force
+		elif on_wall:
+			velocity.y = jump_force
+			velocity.x = -wall_direction * run_speed
+			
+		elif jumps_left > 0:
+			velocity.y = jump_force
+			jumps_left -= 1
 		
 	if Input.is_action_just_released("jump"):
 		velocity.y *= decelerate_on_jump_release
+		
+		
+		
 	
 	#Handle speed/movements.
 	var speed 
@@ -55,7 +77,29 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		
 	move_and_slide()
+	
+	on_wall = false
+	if is_on_wall_only():
+		on_wall = true
+		wall_direction = sign(get_last_slide_collision().get_normal().x)
+	if on_wall and not is_on_floor() and velocity.y > 0 and not basicattack:
+		is_wall_clinging = true
+		sprite.rotation_degrees = wall_direction * -15
+		sprite.scale = Vector2(0.9, 1.1)
+		sprite.position.x = wall_direction * -6
+		player_animation.play("idle")
+		player_animation.flip_h = wall_direction == 1
+	else:
+		is_wall_clinging = false
+		sprite.rotation_degrees = 0
+		sprite.scale = Vector2(1, 1)
+		sprite.position.x = 0
+	
+	
+	
 	if basicattack:
+		pass
+	elif is_wall_clinging:
 		pass
 	else:
 		if direction != 0:
