adf8f05102bc586f221314763ce59be12fdfbea9
Rich D'Costa <rich.dcosta@cathcollege.school.nz>
2026-06-22 09:17:16 +1200

    got portal

---
+++ scripts/next_level_portal.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		var current_scene_file = get_tree().current_scene.scene_file_path
+		var next_level_number = current_scene_file.to_int() + 1
+		
+		var next_level_path = "res://level/" + str(next_level_number) + ".tscn"
