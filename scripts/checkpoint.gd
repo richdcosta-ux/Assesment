02e863c241260d45219f11e68edc4a6ccc206633
richdcosta-ux <rich.dcosta@cathcollege.school.nz>
2026-09-17 12:28:22 +1200

    changes in and a checkpoint scene

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -1,15 +1,15 @@
-extends Node2D
+extends Area2D
 
-var player_spawn_x: float = 59.00
-var player_spawn_y: float = 702.00
-var items = []
-var collectedItems = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player_spawn_x  = 59.00
-	player_spawn_y = 702.00
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	global_position = body.global_position
